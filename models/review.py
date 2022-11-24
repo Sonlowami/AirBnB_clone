f7b01c591c929d0a6d41858e37c3b5625fa82538
kenza1525 <kerianenzabampema@gmail.com>
2022-11-24 06:58:41 -0800

    Added place.py and review.py files inside models directory

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+
+Inherites from BaseModel
+"""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """
+    Class Review
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
