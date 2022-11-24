1f7a8638213ca02a6f9d22a0f5605df3a3dc12ec
kenza1525 <kerianenzabampema@gmail.com>
2022-11-24 06:29:23 -0800

    Added amenity.py, city.py and state.py files

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+Inherites from BaseModel
+"""
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    Class City
+    """
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+Inherites from BAseModel
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State Class
+    """
+    name = ""
