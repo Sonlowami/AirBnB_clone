5f5c6b0e09fc4c2c6ab43875961836a8d33edc91
Sonlowami <uwilowami64@outlook.com>
2022-11-25 19:33:36 +0200

    Save progress

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ Command Enterpreter entry point
 """
 
 import cmd
+import models
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -14,6 +15,7 @@ class HBNBCommand(cmd.Cmd):
     """
     
     prompt = "(hbnb) "
+    classes = {"BaseModel" : models.base_model.BaseModel}
 
     def do_quit(self, line):
         """exit"""
@@ -26,6 +28,17 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         "Do nothing"
         pass
+
+    def do_create(self, line):
+        """Create a BaseModel object"""
+        if not line:
+            print("** class name missing **")
+        elif line in self.classes.keys():
+            obj = self.classes[line]()
+            models.storage.save()
+            print(obj.id)
+        else:
+            print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
