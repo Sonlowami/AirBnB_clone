fea86eef39d7bcac8d44ad6398f6a0fe9b8834ba
Sonlowami <uwilowami64@outlook.com>
2022-11-25 16:04:55 +0200

    Fix a bug in FileStorage
    This fixes a bug with saving and retrieving from json. Previously,
    theprogram only read and kept the most previous json object to the file.
    
    This upgrade keeps the whole history of objects created and saved.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,32 +14,31 @@ class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
-
+    classes = {"BaseModel" : BaseModel}
     def all(self):
         """Return all objects created and saved"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """Add a new object to the object dictionary"""
         if not isinstance(obj, BaseModel):
             return
-        FileStorage.__objects["{self.__class__.__name__}.{self.id}"] = obj
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         """Serialize objects to json file"""
-        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
-            dic = {}
-            for key, obj in FileStorage.__objects.items():
-                dic[key] = obj.to_dict()
+        dic = {}
+        for key, obj in self.__objects.items():
+            dic[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding="utf-8") as f:
             json.dump(dic, f)
 
     def reload(self):
         """Deserialize a json object from a file into a dictionary"""
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path) as f:
+        if os.path.isfile(self.__file_path):
+            with open(self.__file_path) as f:
                 rd = f.read()
                 if rd:
                     for key, obj in json.loads(rd).items():
-                        if key == '__class__':
-                            continue
-                        FileStorage.__objects[key] = obj
+                        objec = self.classes[obj['__class__']]
+                        self.__objects[key] = objec(**obj)
