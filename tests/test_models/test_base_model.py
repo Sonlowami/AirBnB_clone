caab08a84aa19cf35b8ebce3d23a04499732d434
Sonlowami <uwilowami64@outlook.com>
2022-11-28 20:50:42 +0200

    Debug testfile for TestBase

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,16 +11,20 @@ class TestBaseModel(unittest.TestCase):
     Test for BaseModel Class Methods
     """
     @classmethod
-    def setup(cls):
+    def setUp(self):
         """set up an object for test"""
-        cls.my_model = BaseModel()
-        cls.my_model.name = "Holberton"
-        cls.my_model.my_number = 89
+        self.my_model = BaseModel()
+        self.my_model.name = "Holberton"
+        self.my_model.my_number = 89
+
+        self.objct = BaseModel()
+        self.objct.name = "Keriane"
+        self.objct.my_number = 2022
 
     @classmethod
-    def teardown(cls):
+    def tearDown(self):
         """Delete the object at the end of test"""
-        del cls.my_model
+        del self.my_model
 
     def test_str(self):
         """Test if ___str__ show the right output"""
@@ -29,14 +33,15 @@ class TestBaseModel(unittest.TestCase):
 
     def test_save(self):
         """Test if updates are made when changes occur"""
+        self.objct.save()
         self.assertNotEqual(self.objct.created_at, self.objct.updated_at)
 
     def test_to_dict(self):
         """"Test if updated_at and created_at changed their format
             Test if key __clas__ has a value BaseModel"""
-        self.object_dict = self.object.to_dict()
-        self.assertEqual(self.objct_dict['updated_at'], self.object.updated_at.isoformat())
-        self.assertEqual(self.objct_dict['created_at'], self.object.created_at.isoformat())
-        self.assertEqual(self.object.__class__.name, 'BaseModel')
+        objct_dict = self.objct.to_dict()
+        self.assertEqual(objct_dict['updated_at'], self.objct.updated_at.isoformat())
+        self.assertEqual(objct_dict['created_at'], self.objct.created_at.isoformat())
+        self.assertEqual(self.objct.__class__.__name__, 'BaseModel')
 if __name__ == '__main__':
     unittest.main()
